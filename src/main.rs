083b4705a9c407d2c2e4a19436165d44e57c2d8a
d <d.mirauta@protonmail.com>
2023-09-03 14:19:06 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,126 @@
 extern crate ocl;
-use std::{fs, path::Path};
-
+use eframe::egui;
 use ocl::{builders::ProgramBuilder, Buffer, ProQue};
+use std::{fs, path::Path};
 
 static OCL_SOURCE: &str = "./src/ocl/mandel.cl";
 
-struct CFPParam;
+struct BBox {
+    left: f64,
+    right: f64,
+    bot: f64,
+    top: f64,
+}
 
-fn main() -> ocl::Result<()> {
-    let imdim = (120, 30);
+struct Complex {
+    re: f64,
+    im: f64,
+}
 
-    let src =
-        fs::read_to_string(OCL_SOURCE).expect(format!("could not load {OCL_SOURCE}").as_str());
-    let mut prog_build = ProgramBuilder::new();
-    prog_build.src(src).cmplr_opt("-I./src/ocl");
-    dbg!(prog_build.get_compiler_options().unwrap());
-    let pro_que = ProQue::builder()
-        .prog_bldr(prog_build)
-        .dims((imdim.1, imdim.0))
-        .build()?;
+struct FParam {
+    mandel: i32,
+    c: Complex,
+    view_rect: BBox,
+    max_iter: i32,
+}
 
-    // automatically sized like workgroup
-    let iters_buff = pro_que.create_buffer::<i32>()?;
+struct MyApp {
+    imdims: (usize, usize),
+    pro_que: ProQue,
+    iters_buff: Buffer<i32>,
+    iters_vec: Vec<i32>,
+    fparam: FParam,
+}
 
-    // let fp_param = Buffer::<CFPParam>::builder()
-    //     .queue(pro_que.queue().clone())
-    //     .len(1)
-    //     .fill_val(Default::default())
-    //     .build();
+impl Default for MyApp {
+    fn default() -> Self {
+        let imdims = (120, 30);
 
-    // let kernel = pro_que
-    //     .kernel_builder("escape_iter")
-    //     .arg(&iters)
-    //     .arg(&fp_param)
-    //     .build()?;
+        let src =
+            fs::read_to_string(OCL_SOURCE).expect(format!("could not load {OCL_SOURCE}").as_str());
+        let mut prog_build = ProgramBuilder::new();
+        prog_build.src(src).cmplr_opt("-I./src/ocl");
+        dbg!(prog_build.get_compiler_options().unwrap());
+        let pro_que = ProQue::builder()
+            .prog_bldr(prog_build)
+            .dims((imdims.1, imdims.0))
+            .build()
+            .expect("proque error");
 
-    let MAXITER: i32 = 1000;
+        // automatically sized like workgroup
+        let iters_buff = pro_que.create_buffer::<i32>().expect("buffer create error");
 
-    let kernel = pro_que
-        .kernel_builder("escape_iter_args")
-        .arg(&iters_buff)
-        .arg(-2f64) //left
-        .arg(0.5f64) //right
-        .arg(-1.1f64) // bot
-        .arg(1.1f64) // top
-        .arg(0f64) // cre
-        .arg(0f64) // cim
-        .arg(1) // mandel
-        .arg(MAXITER)
-        .build()?;
+        // let fp_param = Buffer::<CFPParam>::builder()
+        //     .queue(pro_que.queue().clone())
+        //     .len(1)
+        //     .fill_val(Default::default())
+        //     .build();
 
-    unsafe {
-        kernel.enq()?;
+        // let kernel = pro_que
+        //     .kernel_builder("escape_iter")
+        //     .arg(&iters)
+        //     .arg(&fp_param)
+        //     .build()?;
+
+        let fparam = FParam {
+            mandel: 1,
+            c: Complex { re: 0.0, im: 0.0 },
+            view_rect: BBox {
+                left: -2.0,
+                right: 0.5,
+                bot: -1.1,
+                top: 1.1,
+            },
+            max_iter: 1000,
+        };
+
+        let buff_len = iters_buff.len();
+        Self {
+            imdims,
+            pro_que,
+            iters_buff,
+            iters_vec: vec![0i32; buff_len],
+            fparam,
+        }
     }
+}
 
-    let mut iters_vec = vec![0i32; iters_buff.len()];
-    iters_buff.read(&mut iters_vec).enq()?;
+impl MyApp {
+    fn run_kernel(&mut self) -> ocl::Result<()> {
+        let kernel = self
+            .pro_que
+            .kernel_builder("escape_iter_args")
+            .arg(&self.iters_buff)
+            .arg(self.fparam.view_rect.left)
+            .arg(self.fparam.view_rect.right)
+            .arg(self.fparam.view_rect.bot)
+            .arg(self.fparam.view_rect.top)
+            .arg(self.fparam.c.re)
+            .arg(self.fparam.c.im)
+            .arg(self.fparam.mandel) // mandel
+            .arg(self.fparam.max_iter)
+            .build()?;
+
+        unsafe {
+            kernel.enq()?;
+        }
+
+        self.iters_buff.read(&mut self.iters_vec).enq()?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut myapp = MyApp::default();
+
+    myapp.run_kernel().unwrap();
 
     // dbg!(iters_vec);
-    let (width, height) = imdim;
+    let (width, height) = myapp.imdims;
     for i in 0..height {
         for j in 0..width {
-            if iters_vec[i * width + j] > MAXITER - 5 {
+            if myapp.iters_vec[i * width + j] > myapp.fparam.max_iter - 5 {
                 print!("*");
             } else {
                 print!(" ");
@@ -69,6 +128,4 @@ fn main() -> ocl::Result<()> {
         }
         println!();
     }
-
-    Ok(())
 }
